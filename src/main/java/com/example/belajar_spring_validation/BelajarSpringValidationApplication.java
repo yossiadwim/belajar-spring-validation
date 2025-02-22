1ea811e5aa2394d28895b28230bff04143f236fd
Yossia Dwi Mahardika <yossia.dwi@ti.ukdw.ac.id>
2025-02-22 18:44:51 +0700

    Initial commit

---
+++ src/main/java/com/example/belajar_spring_validation/BelajarSpringValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/belajar_spring_validation/BelajarSpringValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
